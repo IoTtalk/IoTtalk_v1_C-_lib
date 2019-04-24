ea08a00ec25c32e3b864832190edbfd08bf11d94
Chris Hsu <crishsu.hp@outlook.com>
2019-04-24 12:07:19 +0800

    FIX API

--- IoTTalkLibrary/IoTTalkCustom.cs
+++ IoTTalkLibrary/IoTTalkCustom.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
-using IoTTalkLib.Library;
-using IoTTalkLib.Model;
-using System.Net.NetworkInformation;
 
 namespace IoTTalkLib.Library
 {
-
     class IoTTalkCustom
     {
+        public string Mac_Addr = "";
+        public string SSLIoTTalkServer = "https://iottalk.chu.edu.tw"; //For SSL/TLS/HTTPS
+        public string IoTTalkServer = "http://iottalk.chu.edu.tw"+":9999"; //For SSL/TLS/HTTPS
+        public string Device_Name = "TestDevice";
+        public string Device_Model = "Dummy_Device";
+        public List<string> Device_Feature;
+        public string passwordkey = "";
+
+        public List<string> InputDeviceFeature()
+        {
+            List<string> IDF = new List<string>();
+            IDF.Add("Dummy_Sensor");
+            return IDF;
+        }
+        public List<string> OutputDeviceFeature()
+        {
+            List<string> ODF = new List<string>();
+            ODF.Add("Dummy_Control");
+            return ODF;
+        }
+
         public string MACAddressAuto()
         {
             NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
@@ -21,41 +39,6 @@ namespace IoTTalkLib.Library
                 if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
                     macList.Add(nic.GetPhysicalAddress().ToString());
             return macList[0];
-        }
-
-        public string DeviceMac()
-        {
-            string deviceMac = MACAddressAuto();
-            if (deviceMac == null)
-                deviceMac = "80AC842437";
-            return deviceMac;
-        }
-        public string HostServerIP()
-        {
-            string hostServerIP = "https://ServerIP";
-            return hostServerIP;
-        }
-        public CustomSetModel custom()
-        {
-            CustomSetModel CSM = new CustomSetModel();
-            CSM.deviceName = "D1";
-            CSM.deviceModel = "MorSensor";
-            CSM.passwordkey = "bedd1388-d136-406a-92b4-f78c35d729dc";
-            return CSM;
-        }
-        public List<string>InputDeviceFeature()
-        {
-            List<string> IDF = new List<string>();
-            IDF.Add("Tempature");
-            IDF.Add("Humidity");
-            return IDF;
-        }
-        public List<string> OutputDeviceFeature()
-        {
-            List<string> ODF = new List<string>();
-            ODF.Add("Display");
-            ODF.Add("Print");
-            return ODF;
         }
     }
 }
